4de95e94504ee7a281f5eafc55bf4e375a37c870
andrejstrashun <andrejstrashun@gmail.com>
2021-02-26 00:28:17 +0200

    7 urok

--- src/App.js
+++ src/App.js
@@ -4,17 +4,28 @@ import './App.css';
 const App = () => {
   return (
     <div>
-      <div>One div</div>
-      <div className="App">
-        <ul>
-          <li>css</li>
-          <li>html</li>
-          <li>js</li>
-          <li>react</li>
-        </ul>
-      </div>
+      <Header />
+      <Technologies />
     </div>
   );
+}
+const Technologies = () => {
+  return (<div>
+    <ul>
+      <li>css</li>
+      <li>html</li>
+      <li>js</li>
+      <li>react</li>
+    </ul>
+  </div>);
+}
+
+const Header = () => {
+  return (<div>
+          <a href="#1">Home</a>
+          <a href="#2">News Feed</a>
+          <a href="#3">Messages</a>
+          </div>);
 }
 
 export default App;

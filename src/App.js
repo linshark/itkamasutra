9ce835096bb95b057d3c1614db9bf90789fa92b1
andrejstrashun <andrejstrashun@gmail.com>
2021-02-27 01:17:11 +0200

    9 urok

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Technologies from './Technologies';
+import Header from './Header';
 
 const App = () => {
   return (
@@ -8,24 +10,6 @@ const App = () => {
       <Technologies />
     </div>
   );
-}
-const Technologies = () => {
-  return (<div>
-    <ul>
-      <li>css</li>
-      <li>html</li>
-      <li>js</li>
-      <li>react</li>
-    </ul>
-  </div>);
-}
-
-const Header = () => {
-  return (<div>
-          <a href="#1">Home</a>
-          <a href="#2">News Feed</a>
-          <a href="#3">Messages</a>
-          </div>);
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (<div>
+            <a href="#1">Home</a>
+            <a href="#2">News Feed</a>
+            <a href="#3">Messages</a>
+            </div>);
+  }
+
+  export default Header;

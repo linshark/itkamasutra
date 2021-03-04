264b593218b73827031f761577a3f2d2077a2e39
andrejstrashun <andrejstrashun@gmail.com>
2021-03-05 01:47:21 +0200

    14 urok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import classes from './Header.module.css';
 const Header = () => {
     return <header className='header'>
     <img src='/img/logolotus.jpg'></img>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,3 +1,4 @@
+import classes from './Profile.module.css';
 const Profile = () => {
     return <div className='content'>
     <div><img src='/img/Best-Tropical-Nature-Wallpaper-Background-Beach-Desktop.jpg'></img></div>
@@ -5,8 +6,10 @@ const Profile = () => {
   <div>
     My posts
     <div>New post</div>
-    <div>Post 1</div>
-    <div>Post 2</div>
+    <div className='posts'>
+      <div className='item'>Post 1</div>
+      <div className='item'>Post 2</div>
+    </div>
   </div>
   </div>
 }

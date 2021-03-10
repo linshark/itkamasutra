dea6946a0b6f1653a63a1cc38cc1908b2d7fab90
linshark <53095343+linshark@users.noreply.github.com>
2021-03-10 02:14:09 +0200

    16 14 2

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,21 @@
 import s from './Navbar.module.css';
-
+console.log(s);
 // let s = {
 //   'nav' : 'Navbar_Navbar_nav__2imOE__1NmfI',
-//   'item' : 'Navbar_Navbar_item__zmMk4__1aNQD'
+//   'item' : 'Navbar_Navbar_item__zmMk4__1aNQD',
+//   'active' : 'Baksndakdn_actve'
 // }
+
+//let c1 = "item";
+//let c2 = "active";
+// "item active"
+//let classes = c1 + " " + c2;
+//let classesNew = `${s.item} ${s.active}`;
 
 const Navbar = () => {
     return <nav className={s.nav}>
     <div className={s.item}><a>Profile</a></div>
-    <div className={s.item}><a>Messages</a></div>
+    <div className={`${s.item} ${s.active}`}><a>Messages</a></div>
     <div className={s.item}><a>News</a></div>
     <div className={s.item}><a>Music</a></div>
     <div className={s.item}><a>Settings</a></div>

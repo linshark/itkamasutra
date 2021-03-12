aef240902e8b8039bcfe33f27d531b835f7b9744
linshark <53095343+linshark@users.noreply.github.com>
2021-03-13 01:39:12 +0200

    19 urok

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,23 @@
 import s from './Navbar.module.css';
 console.log(s);
-// let s = {
-//   'nav' : 'Navbar_Navbar_nav__2imOE__1NmfI',
-//   'item' : 'Navbar_Navbar_item__zmMk4__1aNQD',
-//   'active' : 'Baksndakdn_actve'
-// }
-
-//let c1 = "item";
-//let c2 = "active";
-// "item active"
-//let classes = c1 + " " + c2;
-//let classesNew = `${s.item} ${s.active}`;
 
 const Navbar = () => {
     return <nav className={s.nav}>
-    <div className={s.item}><a>Profile</a></div>
-    <div className={`${s.item} ${s.active}`}><a>Messages</a></div>
-    <div className={s.item}><a>News</a></div>
-    <div className={s.item}><a>Music</a></div>
-    <div className={s.item}><a>Settings</a></div>
+    <div className={s.item}>
+      <a href="/profile">Profile</a>
+    </div>
+    <div className={`${s.item} ${s.active}`}>
+      <a href="/dialogs">Messages</a>
+    </div>
+    <div className={s.item}>
+        <a href="/news">News</a>
+      </div>
+    <div className={s.item}>
+        <a href="/music">Music</a>
+      </div>
+    <div className={s.item}>
+        <a href="/settings">Settings</a>
+      </div>
   </nav>
 }
 

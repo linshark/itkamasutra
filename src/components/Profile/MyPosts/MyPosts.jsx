e9240e96770e171e1942ea9da834884421217bc8
andrejstrashun <andrejstrashun@gmail.com>
2021-03-18 01:18:38 +0200

    24 urok

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,12 @@
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 const MyPosts = (props) => {
+
+  let postData = [
+    {id:1, message: 'Hi, how are you?', likesCount:12},
+    {id:2, message: 'It\'s my first post', likesCount:11}
+]
+
   return (
     <div className={s.postsBlock}>
       <h3>My posts</h3>
@@ -13,8 +19,8 @@ const MyPosts = (props) => {
         </div>
       </div>
       <div className={s.posts}>
-        <Post message='Hi, how are you?' likesCount='0' />
-        <Post message='It`s my first post' likesCount='23' />
+        <Post message={postData[0].message} likesCount={postData[0].likesCount} />
+        <Post message={postData[1].message} likesCount={postData[1].likesCount} />
       </div>
     </div>
   )

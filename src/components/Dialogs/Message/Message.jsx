839a9ef84d830c3201988be6d125778e6bf31476
andrejstrashun <andrejstrashun@gmail.com>
2021-03-20 01:49:34 +0200

    26 urok nedodelka

---
+++ src/components/Dialogs/Message/Message.jsx
@@ -0,0 +1,8 @@
+import { NavLink } from 'react-router-dom';
+import s from '../../Dialogs.module.css';
+
+const Message = (props) => {
+    return <div className={s.dialog}>{props.message}</div>
+}
+
+export default Message;

6b6215f179b74f44ea3732716f55791a43ac1181
andrejstrashun <andrejstrashun@gmail.com>
2021-03-21 01:45:19 +0200

    27 urok

--- src/App.js
+++ src/App.js
@@ -15,8 +15,10 @@ const App = (props) => {
         <Header />
         <Navbar />
         <div className='app-wrapper-content'>
-          <Route path='/profile' component={Profile} />
-          <Route path='/dialogs' component={Dialogs} />
+          {/* <Route path='/profile' component={Profile} /> */}
+          <Route path='/profile' render={ () => <Profile /> } />
+          {/* <Route path='/dialogs' component={Dialogs} /> */}
+          <Route path='/dialogs' render={ () => <Dialogs /> } />
           <Route path='/news' component={News} />
           <Route path='/music' component={Music} />
           <Route path='/settings' component={Settings} />

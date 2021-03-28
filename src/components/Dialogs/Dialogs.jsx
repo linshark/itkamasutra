5ff6ad37e21d65e58583b36f4048543cf75df9c4
linshark <andrejstrashun@gmail.com>
2021-03-29 02:30:26 +0300

    31 dop

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,14 +2,18 @@ import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
 import DialogItem from './DialogItem/DialogItem';
 import Message from './Message/Message';
+import React from 'react';
 
 const Dialogs = (props) => {
-
 
     let dialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id} /> ); 
 
     let messagesElements = props.state.messages.map (m => <Message message=<a>{m.message}</a>/>);
-
+    let textEl = React.createRef();
+    let textViv = () => {
+        let mes = textEl.current.value;
+        alert(mes);
+    }
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
@@ -17,6 +21,7 @@ const Dialogs = (props) => {
             </div>
             <div className={s.messages}>
                 {messagesElements}
+                <textarea ref={textEl}></textarea><button onClick={textViv}>Send</button>
             </div>
         </div>
     )

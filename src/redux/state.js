8e3759be138a72f4ec254cedc22ef742385b251c
linshark <andrejstrashun@gmail.com>
2021-04-01 03:34:22 +0300

    33 ur nedodel 11 min

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,5 @@
+import { rerenderEntireTree } from "../render";
+
 let state = {
     profilePage: {
       posts: [
@@ -34,5 +36,6 @@ export let addPost = (postMessage) => {
   };
 
   state.profilePage.posts.push(newPost);
+  rerenderEntireTree(state);
 }
 export default state;

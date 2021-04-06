76b2f71ac1340137841248086af098c11536eabb
linshark <andrejstrashun@gmail.com>
2021-04-07 01:12:09 +0300

    37 urok nachalo

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,34 @@
+let store={
+  _state : {
+    profilePage: {
+      posts: [
+          {id:1, message: 'Hi, how are you?', likesCount:12},
+          {id:2, message: 'It\'s my first posts', likesCount:11},
+          {id:3, message: 'Hi, how are you?', likesCount:14},
+          {id:4, message: 'Hi, how are you?', likesCount:16}
+      ],
+      newPostText: 'it-kamasutra.com'
+    },
+    dialogsPage: {
+      messages: [
+          {id:1, message: 'Hi'},
+          {id:2, message: 'How is your it-kamasutra?'},
+          {id:3, message: 'Yo'},
+          {id:4, message: 'Yo'},
+          {id:5, message: 'Yo'}
+      ],
+      dialogs: [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Andrey'},
+        {id: 3, name: 'Sveta'},
+        {id: 4, name: 'Sasha'},
+        {id: 5, name: 'Viktor'},
+        {id: 6, name: 'Valera'}
+      ]
+    },
+    sidebar:{}
+  }
+}
 let rerenderEntireTree = () => {
   console.log('State was changed');
 }

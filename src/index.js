26bdbd6bb6fd580c76b3e240b8f226d8eab493fe
linshark <andrejstrashun@gmail.com>
2021-04-11 02:11:12 +0300

    ur 37 second 9_43

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import reportWebVitals from './reportWebVitals';
-import state, {subscribe} from './redux/state';
+import store from './redux/state';
 //addPost('SamuraiJS.COM');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {addPost, updateNewPostText} from './redux/state';
 import {BrowserRouter} from 'react-router-dom';
 
 
@@ -14,16 +13,16 @@ let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <React.StrictMode>
       <BrowserRouter>
-        <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />
+        <App state={store.getState()} addPost={store.addPost} updateNewPostText={store.updateNewPostText} />
       </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
   );
 }
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- src/redux/state.js
+++ src/redux/state.js
@@ -27,64 +27,32 @@ let store={
       ]
     },
     sidebar:{}
+  },
+  getState(){
+    return this._state;
+  },
+  _callSubscriber(){
+    console.log('State was changed');
+  },
+  addPost(){
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0
+    };
+  
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText = '';
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText){
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  subscribe(observer){
+    this._callSubscriber = observer; //Паттерн наблюдатель Observer
   }
 }
-let rerenderEntireTree = () => {
-  console.log('State was changed');
-}
-
-let state = {
-    profilePage: {
-      posts: [
-          {id:1, message: 'Hi, how are you?', likesCount:12},
-          {id:2, message: 'It\'s my first posts', likesCount:11},
-          {id:3, message: 'Hi, how are you?', likesCount:14},
-          {id:4, message: 'Hi, how are you?', likesCount:16}
-      ],
-      newPostText: 'it-kamasutra.com'
-    },
-    dialogsPage: {
-      messages: [
-          {id:1, message: 'Hi'},
-          {id:2, message: 'How is your it-kamasutra?'},
-          {id:3, message: 'Yo'},
-          {id:4, message: 'Yo'},
-          {id:5, message: 'Yo'}
-      ],
-      dialogs: [
-        {id: 1, name: 'Dimych'},
-        {id: 2, name: 'Andrey'},
-        {id: 3, name: 'Sveta'},
-        {id: 4, name: 'Sasha'},
-        {id: 5, name: 'Viktor'},
-        {id: 6, name: 'Valera'}
-      ]
-    },
-    sidebar:{}
-}
-window.state=state;
-
-export const addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likesCount: 0
-  };
-
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText = '';
-  rerenderEntireTree(state);
-}
-
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer; //Паттерн наблюдатель Observer
-}
-
-export default state;
-
+export default store;
+window.store=store;
 // store - OOP

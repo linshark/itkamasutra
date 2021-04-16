c9906b1da62dd38e246e8a0deda149ad09f3b64c
linshark <andrejstrashun@gmail.com>
2021-04-17 02:53:24 +0300

    38 ur 16_53

--- src/redux/state.js
+++ src/redux/state.js
@@ -32,6 +32,9 @@ let store={
     //debugger;
     return this._state;
   },
+  subscribe(observer){
+    this._callSubscriber = observer; //Паттерн наблюдатель Observer
+  },
   _callSubscriber(){
     console.log('State was changed');
   },
@@ -51,8 +54,20 @@ let store={
     this._state.profilePage.newPostText = newText;
     this._callSubscriber(this._state);
   },
-  subscribe(observer){
-    this._callSubscriber = observer; //Паттерн наблюдатель Observer
+  dispatch(action){//{type: 'ADD-POST'}
+    if(action.type === 'ADD-POST'){
+      let newPost = {
+        id: 5,
+        message: this._state.profilePage.newPostText,
+        likesCount: 0
+    };
+      this._state.profilePage.posts.push(newPost);
+      this._state.profilePage.newPostText = '';
+      this._callSubscriber(this._state);
+        } else if (action.type === 'UPDATE-NEW-POST-TEXT'){
+      this._state.profilePage.newPostText = action.newText;
+      this._callSubscriber(this._state);
+    }
   }
 }
 export default store;
